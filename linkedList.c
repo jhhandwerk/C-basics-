42d8113bc7736217b0bd63dbed965c642a3d7fc2
Jonathan Handwerk <jonathanhandwerk@Jonathans-MacBook-Air.local>
2022-07-23 00:17:06 +0900

    making linked list with three  nodes

--- linkedList.c
+++ linkedList.c
@@ -16,6 +16,13 @@ int main() {
     head->data = 98;
     current->link = NULL;
     head->link = current;
+
+    current = malloc(sizeof(struct node));
+    current->data = 3;
+    current->link = NULL;
+
+    head->link->link = current;
+
     return 0;
 }
 
