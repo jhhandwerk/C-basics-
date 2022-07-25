9c68b225b4c0f6bda2f0884b3a619ca415165a49
Jonathan Handwerk <jonathanhandwerk@Jonathans-MacBook-Air.local>
2022-07-26 01:52:21 +0900

    made a loop to add nodes to the list

--- LLfunciton.c
+++ LLfunciton.c
@@ -17,21 +17,41 @@ void printList(node_t *head){
     }
 }
 
+node_t *create_new_node(int value){
+    node_t *result = malloc(sizeof(node_t));
+    result->value = value;
+    result->next = NULL;
+    return result;
+}
+
 int main(){
-    node_t n1,n2,n3;
-    node_t *head;
+    node_t *head = NULL;
+    node_t *tmp;
 
-    n1.value = 45;
-    n2.value = 8;
-    n3.value = 32;
-
-    // link them
-    head = &n3;
-    n3.next = &n2;
-    n2.next = &n1;
-    n1.next = NULL;
+    for(int i=0; i < 25; i++){
+        tmp = create_new_node(i);
+        tmp->next = head;
+        head = tmp;
+    }
     printList(head);
-
     return 0;
 }
+
+// int main(){
+//     node_t *head;
+//     node_t *tmp;
+
+//     tmp = create_new_node(32);
+//     head = tmp;
+//     tmp = create_new_node(64);
+//     tmp->next = head;
+//     head = tmp;
+//     tmp = create_new_node(82);
+//     tmp->next = head;
+//     head = tmp;
+
+//     printList(head);
+
+//     return 0;
+// }
 
